c596f1c1dc2259cd0387ef60aa6f9e13e86251b2
Arina <hadson.rey@mail.ru>
2020-06-18 17:27:19 +1100

    Отображает в карточке места остальные моковые данные. Обозначает type props.

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -1,5 +1,6 @@
 import React, {PureComponent} from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import cn from "classnames";
 
 class PlaceCard extends PureComponent {
   constructor(props) {
@@ -13,12 +14,14 @@ class PlaceCard extends PureComponent {
 
     return (
       <article className="cities__place-card place-card">
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
+
+        {
+          place.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ``
+        }
+
         <div className="cities__image-wrapper place-card__image-wrapper">
           <a href="#">
-            <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+            <img className="place-card__image" src={place.img} width="260" height="200" alt="Place image"/>
           </a>
         </div>
         <div className="place-card__info">
@@ -27,7 +30,7 @@ class PlaceCard extends PureComponent {
               <b className="place-card__price-value">&euro;{place.price}</b>
               <span className="place-card__price-text">&#47;&nbsp;night</span>
             </div>
-            <button className="place-card__bookmark-button button" type="button">
+            <button className={cn(`place-card__bookmark-button button`, {"place-card__bookmark-button--active": place.isBookmark})} type="button">
               <svg className="place-card__bookmark-icon" width="18" height="19">
                 <use xlinkHref="#icon-bookmark" />
               </svg>
@@ -36,7 +39,7 @@ class PlaceCard extends PureComponent {
           </div>
           <div className="place-card__rating rating">
             <div className="place-card__stars rating__stars">
-              <span style={{width: `80%`}} />
+              <span style={{width: place.rating}} />
               <span className="visually-hidden">Rating</span>
             </div>
           </div>
@@ -61,7 +64,12 @@ PlaceCard.propTypes = {
   place: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired
+    price: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    isBookmark: PropTypes.bool.isRequired
   })
 };
 
