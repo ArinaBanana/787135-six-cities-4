ff215a73aa49d3b58d628ed7344edcf7fdcd2580
Arina <hadson.rey@mail.ru>
2020-06-19 15:43:07 +1100

    Создает функцию декоратор для замедления вызовов функций.

--- src/utils/func.js
+++ src/utils/func.js
@@ -12,4 +12,33 @@ const getRandomBoolean = () => {
   return Boolean(getRandomNumber(0, 2));
 };
 
-export {getRandomArrayItem, getRandomNumber, getRandomBoolean};
+function throttle(func, ms) {
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+export {getRandomArrayItem, getRandomNumber, getRandomBoolean, throttle};
