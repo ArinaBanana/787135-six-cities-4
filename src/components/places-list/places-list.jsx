c1840d5f4853e0d777f11ab55c3eb55962dce75e
Arina <hadson.rey@mail.ru>
2020-06-19 15:44:19 +1100

    Оборачивает функцию (вызов ктр происходит на наведение курсора) в тротл. Рефакторинг.

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -1,8 +1,27 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import PlaceCard from "../place-card/place-card.jsx";
+import {throttle} from "../../utils/func";
 
 class PlacesList extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeCard: -1
+    };
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+
+    this.throttledHandleMouseMove = throttle(this.handleMouseMove, 200);
+  }
+
+  handleMouseMove(placeId) {
+    this.setState({
+      activeCard: placeId
+    });
+  }
+
   render() {
     const {places, onTitleClick} = this.props;
 
@@ -12,7 +31,9 @@ class PlacesList extends PureComponent {
           places.map((place) => <PlaceCard
             key={place.id}
             place={place}
-            onTitleClick={onTitleClick}/>)
+            onTitleClick={onTitleClick}
+            onMouseMove={this.throttledHandleMouseMove}
+          />)
         }
       </div>
     );
