920418edaf7c28ea24dfd9f9a1160fda0cd5ac4f
Arina <hadson.rey@mail.ru>
2020-06-19 20:27:09 +1100

    Реализует переключение экранов по пути (для разработки). Переделывает App в классовый компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen.jsx";
+import DetailedInfoScreen from "../detailed-info-screen/detailed-info-screen.jsx";
 
-const titlePlaceHandler = (id) => {
-  return id;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App = ({countPlaces, places}) => {
-  return (
-    <MainScreen
-      countPlaces={countPlaces}
-      places={places}
-      onTitleClick={titlePlaceHandler}
-    />
-  );
-};
+    this.handleTitlePlaceClick = this.handleTitlePlaceClick.bind(this);
+  }
+
+  handleTitlePlaceClick(id) {
+    return id;
+  }
+
+  render() {
+    const {countPlaces, places} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <MainScreen
+              countPlaces={countPlaces}
+              places={places}
+              onTitleClick={this.handleTitlePlaceClick}
+            />
+          </Route>
+          <Route exact path="/dev-detailed">
+            <DetailedInfoScreen />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   countPlaces: PropTypes.number.isRequired,
