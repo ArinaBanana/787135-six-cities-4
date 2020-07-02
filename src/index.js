8a0f5a020e3cbd31959d886ad5c18c4c7a3d024b
Arina <hadson.rey@mail.ru>
2020-07-02 23:18:39 +1100

    Рефакторинг

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import {COUNT_PLACES, generatePlaces} from "./mocks/offers";
+import {generatePlaces} from "./mocks/offers";
 import {reviews} from "./mocks/reviews";
 
 const places = generatePlaces();
 
 ReactDOM.render(
     <App
-      countPlaces={COUNT_PLACES}
+      countPlaces={places.length}
       places={places}
       reviews={reviews}
     />,
