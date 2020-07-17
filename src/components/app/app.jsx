76888096d59dd0720489af798c98513fcdff126c
Arina <hadson.rey@mail.ru>
2020-07-17 22:37:37 +1100

    Refactor

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React, {PureComponent} from "react";
 import {BrowserRouter} from "react-router-dom";
-import Screens from "../screens";
+import Screens from "../screens/importComponent";
 
 class App extends PureComponent {
   render() {
