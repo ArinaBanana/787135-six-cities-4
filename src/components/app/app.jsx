20b46971e88cffd99191284708baec34ae57d223
Arina <hadson.rey@mail.ru>
2020-07-23 13:24:36 +1100

    Переделывает app в функциональный компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,13 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import Screens from "../screens/importComponent";
 
-class App extends PureComponent {
-  render() {
-    return (
-      <BrowserRouter>
-        <Screens />
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Screens/>
+    </BrowserRouter>
+  );
 }
 
 export default App;
