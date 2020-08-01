c2f844325e43c76558cfad9f1bbcea5dbffc4574
Arina <hadson.rey@mail.ru>
2020-08-01 14:06:21 +1100

    Добавляет метод Operation.setPlaces для получения "настоящих" мест с сервера, и функцию адаптирование данных

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,14 +1,61 @@
-import {generatePlaces} from "../../mocks/places";
-const places = generatePlaces();
+const adaptPlaces = (places) => {
+  const getArrayCoordinates = (latitude, longitude) => {
+    let array = [];
+    array.push(latitude, longitude);
+
+    return array;
+  };
+
+  return places.map((place) => {
+    return {
+      id: place.id,
+      title: place.title,
+      price: place.price,
+      img: place[`preview_image`],
+      type: place.type,
+      coordinates: getArrayCoordinates(place.location.latitude, place.location.longitude),
+      zoom: place.zoom,
+      rating: `${place.rating}`,
+      isPremium: place[`is_premium`],
+      isBookmark: place[`is_favorite`],
+      city: {
+        name: place.city.name,
+        coordinates: getArrayCoordinates(place.city.location.latitude, place.city.location.longitude),
+        zoom: place.city.location.zoom,
+      },
+      description: place.description,
+      goods: place.goods,
+      allImages: place.images,
+      adults: place[`max_adults`],
+      host: {
+        avatar: place.host[`avatar_url`],
+        id: place.host.id,
+        isPro: place.host[`is_pro`],
+        name: place.host.name
+      },
+      bedrooms: place.bedrooms
+    };
+  });
+};
 
 const ActionType = {
   SET_PLACES: `SET_PLACES`,
 };
 
 const ActionCreator = {
-  setPlaces() {
+  setPlaces(places) {
     return {type: ActionType.SET_PLACES, payload: places};
   }
 };
 
-export {ActionType, ActionCreator};
+const Operation = {
+  setPlaces: () => (dispatch, getState, api) => {
+    return api.get(`/hotels`).then((response) => {
+      const places = adaptPlaces(response.data);
+
+      dispatch(ActionCreator.setPlaces(places));
+    });
+  }
+};
+
+export {Operation, ActionType, ActionCreator};
