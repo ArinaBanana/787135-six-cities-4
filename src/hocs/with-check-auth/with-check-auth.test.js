492a7f1ee2328f12bcc9e1d613b64a5d83e49ccc
Arina <hadson.rey@mail.ru>
2020-08-10 17:01:53 +1100

    Create test for hoc with check

---
+++ src/hocs/with-check-auth/with-check-auth.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import withCheckAuth from "./with-check-auth";
+
+jest.mock(`react-redux`, () => {
+  return {
+    connect: () => (Component) => Component
+  };
+});
+
+function MockComponent() {
+  return (
+    <div />
+  );
+}
+
+it(`Check that HOC's the callback is being made`, () => {
+  const checkAuth = jest.fn();
+
+  const MockComponentWrapped = withCheckAuth(MockComponent);
+  MockComponentWrapped.prototype.componentDidMount.call({
+    props: {
+      checkAuth
+    }
+  });
+
+  expect(checkAuth).toHaveBeenCalled();
+});
